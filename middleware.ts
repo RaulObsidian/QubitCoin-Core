6a950553932cd442ac07a54f2f6afaef6bbe737a
RaulObsidian <franciscoraulr@gmail.com>
2025-12-07 19:51:08 +0100

    fix(routing): remove root layout conflict and broaden middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -2,4 +2,8 @@ import createMiddleware from 'next-intl/middleware';
 import {routing} from './i18n';
 
 export default createMiddleware(routing);
-export const config = { matcher: ['/', '/(de|en|es)/:path*'] };
+
+export const config = {
+  // Matcher universal que atrapa todo excepto archivos estáticos
+  matcher: ['/((?!api|_next|.*\\..*).*)']
+};
