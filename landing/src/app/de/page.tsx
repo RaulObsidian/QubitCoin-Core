c753bf1450806f9f80023d54c467d75e0327c014
RaulObsidian <franciscoraulr@gmail.com>
2025-12-07 17:22:54 +0100

    fix(critical): remove broken German locale to restore deployment

--- landing/src/app/de/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/de/page.tsx
-import { getMessages } from 'next-intl/server';
-import ClientPage from '../ClientPage';
-
-export default async function GermanPage() {
-  const messages = await getMessages();
-  
-  return <ClientPage locale="de" messages={messages} />;
-}
