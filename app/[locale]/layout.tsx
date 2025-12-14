6791405a2acc591d59471a4534bee6bf4d25ba21
RaulObsidian <franciscoraulr@gmail.com>
2025-12-14 03:13:25 +0100

    revert(layout): remove provider from server layout to fix server-side error

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,9 +1,7 @@
 import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
 import {routing} from '../../i18n';
-import {NextIntlProvider} from 'next-intl';
 
-export default async function LocaleLayout({
+export default function LocaleLayout({
   children,
   params,
 }: {
@@ -17,16 +15,10 @@ export default async function LocaleLayout({
     notFound();
   }
 
-  // Get messages for the current locale
-  const config = await getRequestConfig({locale});
-  const messages = config.messages;
-
   return (
     <html lang={locale}>
       <body className="min-h-screen bg-gradient-to-br from-nebulae-black to-black">
-        <NextIntlProvider locale={locale} messages={messages}>
-          {children}
-        </NextIntlProvider>
+        {children}
       </body>
     </html>
   );
