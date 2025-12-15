a696b01258779efb3096f3ce566dba6db827dcd8
RaulObsidian <franciscoraulr@gmail.com>
2025-12-15 01:18:39 +0100

    fix(config): revert next.config.js to standard minimal setup to fix js loading

--- landing/next.config.js
+++ landing/next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  reactStrictMode: true,
-  trailingSlash: false,
-  distDir: '.next',
-  // Aseguramos que no busque fuera
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  output: "standalone",
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
+
 module.exports = nextConfig;
