ac1dcb8f29f06998041e4372a05458c64d9ba058
RaulObsidian <franciscoraulr@gmail.com>
2026-01-07 16:28:24 +0100

    feat: adjust background image brightness to 1.35

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -448,7 +448,7 @@ while Verification_Fails(Permutation):
             <img
               src="/qbc_hero.png"
               alt="QubitCoin Brand Identity"
-              className="w-[1400px] h-[1400px] object-contain opacity-40 mix-blend-overlay brightness-150 contrast-175"
+              className="w-[1400px] h-[1400px] object-contain opacity-40 mix-blend-overlay brightness-135 contrast-175"
             />
           </div>
         </div>
